5304391a9c6db3d0336d3cc5042b37f3bcbd3b4c
GulamFatima-droid <gulamfatima.ahmadnaz@ada.ac.uk>
2024-09-25 11:30:02 +0000

    First attempt at using objects for the game

--- rps.js
+++ rps.js
@@ -1,8 +1,28 @@
 function rockPaperScissors(player1, player2) {
+  const rock = {
+    weakness : 'paper'
+  }
+  const paper = {
+    weakness : 'scissors'
+  }
+  const scissors = {
+    weakness : 'rock'
+  }
 
-  
-   
+
+  console.log(player1['weakness']);
+
+  if(player1 === player2){
+    return 'draw';
+  }else if(player1['weakness'] === player2 ){
+    return 'player2'
+  }else{
+    return 'player1'
+  }
+     
 }
+
+console.log(rockPaperScissors('paper','scissors'));
 
 
 
