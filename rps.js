c1e5ae322414bc6b896ffea09e990a939f3440dd
GulamFatima-droid <gulamfatima.ahmadnaz@ada.ac.uk>
2024-09-25 13:09:34 +0000

    Tried to shrink my code

--- rps.js
+++ rps.js
@@ -1,42 +1,22 @@
 function rockPaperScissors(player1, player2) {
 
-  const gameChoices = {
-    rock : {
-      weaknesses : ['paper','spock']
-    },
-    paper : {
-      weaknesses : ['scissors','lizard']
-    },
-    scissors : {
-      weaknesses : ['rock','spock']
-    },
-    lizard : {
-      weaknesses : ['rock','scissors']
-    },
-    spock : {
-      weaknesses : ['lizard','paper']
-    }
+  const gameRules = {
+      rockWeaknesses : ['paper','spock'],
+      paperWeaknesses : ['scissors','lizard'],
+      scissorsWeaknesses : ['rock','spock'],
+      lizardWeaknesses : ['rock','scissors'],
+      spockWeaknesses : ['lizard','paper']    
   }
-  
-
-  const player1Choice = gameChoices[player1];
-  const player2Choice = gameChoices[player2];
-
-
-  console.log(player1Choice['weaknesses']);
 
   if(player1 === player2){
     return 'draw';
-  }else if(player1Choice['weaknesses'].includes(player2) ){
-    return 'player2'
-  }else{
+  }else if(gameRules[`${player2}Weaknesses`].includes(player1) ){
     return 'player1'
+  }else{
+    return 'player2'
   }
      
 }
-
-
-
 
 
 
